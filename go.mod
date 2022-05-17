c72cbed5b67240b2c6bf63cb25d186cd3f273800
​Andrzej Ressel <andrzej.ressel@gmail.com>
2022-05-18 00:38:26 +0200

    Add more metrics

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/andrzejressel/syncovery_collector
 
 go 1.18
 
-require github.com/prometheus/client_golang v1.12.1
+require github.com/prometheus/client_golang v1.12.2
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -12,6 +12,6 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.34.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- syncovery_collector.go
+++ syncovery_collector.go
@@ -14,6 +14,12 @@ const (
 type SyncoveryCollector struct {
 	client          *SyncoveryClient
 	LastRunDateTime *prometheus.Desc
+	NextRunDateTime *prometheus.Desc
+	LastRunFailure  *prometheus.Desc
+	IsScheduled     *prometheus.Desc
+	IsLocked        *prometheus.Desc
+	IsDisabled      *prometheus.Desc
+	IsRunning       *prometheus.Desc
 }
 
 func NewSyncoveryCollector(client *SyncoveryClient) *SyncoveryCollector {
@@ -22,8 +28,44 @@ func NewSyncoveryCollector(client *SyncoveryClient) *SyncoveryCollector {
 		client: client,
 
 		LastRunDateTime: prometheus.NewDesc(
-			namespace+"last_run",
+			namespace+"last_run_timestamp_seconds",
 			"Timestamp of last run of profile",
+			[]string{"number", "name"},
+			nil,
+		),
+		NextRunDateTime: prometheus.NewDesc(
+			namespace+"next_run_timestamp_seconds",
+			"Timestamp of next run of profile",
+			[]string{"number", "name"},
+			nil,
+		),
+		LastRunFailure: prometheus.NewDesc(
+			namespace+"last_run_failure",
+			"Displays whether or not the last profile run was a failure",
+			[]string{"number", "name"},
+			nil,
+		),
+		IsScheduled: prometheus.NewDesc(
+			namespace+"scheduled",
+			"Displays whether or not the profile is scheduled",
+			[]string{"number", "name"},
+			nil,
+		),
+		IsRunning: prometheus.NewDesc(
+			namespace+"running",
+			"Displays whether or not the profile is running",
+			[]string{"number", "name"},
+			nil,
+		),
+		IsDisabled: prometheus.NewDesc(
+			namespace+"disabled",
+			"Displays whether or not the profile is disabled",
+			[]string{"number", "name"},
+			nil,
+		),
+		IsLocked: prometheus.NewDesc(
+			namespace+"locked",
+			"Displays whether or not the profile is locked",
 			[]string{"number", "name"},
 			nil,
 		),
@@ -32,12 +74,19 @@ func NewSyncoveryCollector(client *SyncoveryClient) *SyncoveryCollector {
 
 func (sc *SyncoveryCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- sc.LastRunDateTime
+	ch <- sc.LastRunFailure
+	ch <- sc.NextRunDateTime
+	ch <- sc.IsRunning
+	ch <- sc.IsDisabled
+	ch <- sc.IsScheduled
+	ch <- sc.IsLocked
+
 }
 
 func (sc *SyncoveryCollector) Collect(ch chan<- prometheus.Metric) {
 	profiles, err := sc.client.GetProfiles()
 	if err != nil {
-		log.Printf("failed to get torrents: %v", err)
+		log.Printf("failed to get profiles: %v", err)
 		return
 	}
 
@@ -48,6 +97,52 @@ func (sc *SyncoveryCollector) Collect(ch chan<- prometheus.Metric) {
 			float64(v.LastRunDateTime.Unix()),
 			strconv.Itoa(v.Number), v.Name,
 		)
+		ch <- prometheus.MustNewConstMetric(
+			sc.LastRunFailure,
+			prometheus.GaugeValue,
+			float64Bool(v.LastRunHadError),
+			strconv.Itoa(v.Number), v.Name,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			sc.IsLocked,
+			prometheus.GaugeValue,
+			float64Bool(v.IsLocked),
+			strconv.Itoa(v.Number), v.Name,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			sc.IsDisabled,
+			prometheus.GaugeValue,
+			float64Bool(v.IsDisabled),
+			strconv.Itoa(v.Number), v.Name,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			sc.IsRunning,
+			prometheus.GaugeValue,
+			float64Bool(v.IsRunning),
+			strconv.Itoa(v.Number), v.Name,
+		)
+		ch <- prometheus.MustNewConstMetric(
+			sc.IsScheduled,
+			prometheus.GaugeValue,
+			float64Bool(v.IsScheduled),
+			strconv.Itoa(v.Number), v.Name,
+		)
+		if v.NextRunDateTime != nil {
+			ch <- prometheus.MustNewConstMetric(
+				sc.NextRunDateTime,
+				prometheus.GaugeValue,
+				float64(v.NextRunDateTime.Unix()),
+				strconv.Itoa(v.Number), v.Name,
+			)
+		}
 	}
 
+}
+
+func float64Bool(b bool) float64 {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
 }
